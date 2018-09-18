bae045286c033141018da7727164cc70aaa58dae
DESKTOP-5PGTGEF\rohit <rohitnagraj.99@gmail.com>
2018-09-18 07:07:27 +0530

    Added Java lab programs

---
+++ Sem_3/Object_Oriented/Unit_1/Lab_Programs/FibonacciWithoutRecurssion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class FibonacciWithoutRecurssion
+{
+	public static void main(String [] args)
+	{
+		int a=0,b=1,c;
+
+		System.out.print("Enter the limit of the series: ");
+		int limit;
+		Scanner scanner = new Scanner(System.in);
+		limit= scanner.nextInt();
+
+		while(a<limit)
+		{
+			System.out.print(a + " : ");
+			c=a+b;
+			a=b;
+			b=c;
+		}
+		System.out.print("\n");
+	}
+}
