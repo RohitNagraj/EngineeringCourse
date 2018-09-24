8bfd65aab48a761bb0935f60ef69ba0d36a90b19
Rohit Nagraj <41142333+RohitNagraj@users.noreply.github.com>
2018-09-24 10:36:48 +0530

    Add files via upload

---
+++ Sem_3/Object_Oriented/Unit_1/Lab_Programs/FibonacciWithRecurrsion.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class FibonacciWithRecurssion
+{
+	static int fibonacci(int n)
+	{
+		if(n==0)
+			return 0;
+		else if (n==1)
+			return 1;
+		else
+			return (fibonacci(n-1) + fibonacci(n-2));
+	}
+	
+	public static void main(String [] args)
+	{
+		int n,i;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter the series size: ");
+		n=scanner.nextInt();
+		
+		for(i=0;i<n;i++)
+		{
+			System.out.print(fibonacci(i) + " : ");
+		}
+		System.out.println("");
+		
+	}	
+}
