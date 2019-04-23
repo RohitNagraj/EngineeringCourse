9d12a60965a81a05612b19129766eb6c28e67fd4
Rohit Nagraj <41142333+RohitNagraj@users.noreply.github.com>
2019-04-23 10:48:01 +0530

    Update HeapSort.c

--- Semester4/Algorithms/Q5/HeapSort.c
+++ Semester4/Algorithms/Q5/HeapSort.c
@@ -133,13 +133,12 @@ void heapifyDown(int *a, int n, int index)
 void buildMaxHeap(int *a, int n)
 {
     int i;
-    for (i = 3; i >= 0; i--)
-        heapifyDown(a, 7, i);
+    for (i = n/2; i >= 0; i--)
+        heapifyDown(a, n, i);
 }
 
-double heapSort(int *a, int n)
+void heapSort(int *a, int n)
 {
-    clock_t start = clock();
     int i, t = n;
     buildMaxHeap(a, n);
     for (i = n - 1; i > 0; i--)
@@ -147,23 +146,25 @@ double heapSort(int *a, int n)
         swap(a, 0, i);
         heapifyDown(a, --t, 0);
     }
-
-    return ((double)(clock() - start)) / CLOCKS_PER_SEC;
 }
 
 int main()
 {
-    int i, n;
+    int i, n, a[100];
     double t;
     printf("Enter the size of the array: ");
     scanf("%d", &n);
-    int a[n];
 
-    for (i = 0; i < n; i++) // Generate an array with random values
-        a[i] = rand() % 1000;
+    for (i = 0; i < n; i++)
+            scanf("%d", &a[i]);
 
-    t = heapSort(a, n);
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
 
-    printf("Time: %fs\n", t);
+    heapSort(a, n);
+
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
     return 0;
 }
