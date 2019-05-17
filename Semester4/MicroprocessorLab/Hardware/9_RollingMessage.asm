ec3f4abdcdc8d4b664b2ab089901bd8a1271a79f
Rohit Nagraj <rohitnagraj.99@gmail.com>
2019-05-17 20:04:47 +0530

    Added comments

--- Semester4/MicroprocessorLab/Hardware/9_RollingMessage.asm
+++ Semester4/MicroprocessorLab/Hardware/9_RollingMessage.asm
@@ -3,64 +3,74 @@
 
 assume cs:code,ds:data
 data segment
-pa equ 20a0h
-pb equ 20a1h
-pc equ 20a2h
-cr equ 20a3h
-msg db 0d1h,0ffh,0ffh,0ffh,061h,0ffh,0ffh,0ffh,0e3h,0ffh,0ffh,0ffh,031h,0ffh
-
+        pa equ 20a0h
+        pb equ 20a1h
+        pc equ 20a2h
+        cr equ 20a3h
+        msg db 71h, 9fh, 0f5h, 61h, 0ffh,71h, 9fh, 0f5h, 61h, 0ffh,71h, 9fh, 0f5h, 61h, 0ffh ; The message is "FIRE FIRE FIRE"
 data ends
+
 code segment
-start: mov ax,data
-       mov ds,ax
-       mov dx,cr
+start: 
+        mov ax,data
+        mov ds,ax
 
-       mov al,80h
-       out dx,al
-rpt:
-       mov cx,14d
-       lea si,msg
-nextchar:
-       mov al,[si]
-       call disp
-       call delay
-       inc si
-       loop nextchar
-       mov ah,06h
-       mov dl,0ffh
-       int 21h
-       jz rpt
-mov ah,4ch
-int 21h
-      
-disp proc
-        push cx
-        mov cx,08
-        nextbit:
-        mov dx,pb
+        mov dx,cr
+        mov al,80h
         out dx,al
-        push ax
-        mov al,0ffh
-        mov dx,pc
-        out dx,al
-        mov al,00h
-        out dx,al
-        pop ax
-        ror al,1
-        loop nextbit
-        pop cx
-        ret
-disp endp
 
-delay proc
-        mov bx,07fffh
-        l2:
-        mov di,07fffh
-        l1: dec di
-        jnz l1
-        dec bx
-        jnz l2
-        ret
-delay endp
+        rpt:
+                mov cx,15d      ; 15 is the message length
+                lea si,msg
+
+                nextchar:
+                        mov al,[si]
+                        call disp
+                        call delay
+                        inc si
+                        loop nextchar
+                        
+                mov ah,06h      ; Check for keyboard interrupt
+                mov dl,0ffh
+                int 21h
+                jz rpt          ; If none, repeat
+
+        mov ah,4ch
+        int 21h
+
+        disp proc
+                push cx
+                mov cx,08
+                nextbit:
+                        mov dx,pb
+                        out dx,al
+                
+                        push ax
+                        
+                        mov al,0ffh
+                        mov dx,pc
+                        out dx,al
+
+                        mov al,00h
+                        out dx,al
+
+                        pop ax
+                        ror al,1
+                        loop nextbit
+                pop cx
+                ret
+        disp endp
+
+        delay proc
+                mov bx,07fffh
+                l2:
+                        mov di,07fffh
+                        l1: 
+                                dec di
+                                jnz l1
+                        dec bx
+                        jnz l2
+                ret
+        delay endp
 code ends
 end start
